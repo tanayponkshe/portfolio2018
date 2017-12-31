ac5c0f966b82ad8396ea077947bb2fdde1374d0e
Tanay <Tanay@Tanays-MacBook-Pro.local>
2017-12-31 13:40:15 -0800

    react basic project

---
+++ my-app/src/App.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+
+
---
+++ my-app/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()} id={number.toString()}>
+      {number}
+    </li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
+const numbers = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
+);
+
+registerServiceWorker();
